368715cae66ee2101871f3caeff87e4f709a9cd2
Alastair Tse <alastair@liquidx.net>
2021-04-03 22:58:10 +0900

    Using a lower page size to get around photos.albums.list failing.

--- main.js
+++ main.js
@@ -2,38 +2,80 @@ const { authenticatedClient, FileTokenStorage } = require('liquidx-gapi-auth')
 const { program } = require('commander');
 const process = require('process');
 const Photos = require('googlephotos');
+const fs = require('fs');
 
-const listPhotos = (accessToken, pageSize = 50) => {
+const SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly'];
+
+const albumListRecursive = async (accessToken, pageSize = 50) => {
   const photos = new Photos(accessToken)
-  return photos.albums.list(pageSize)
+
+  const getNextAlbumPage = async (albums, nextPageToken) => {
+    const result = await photos.albums.list(pageSize, nextPageToken)
+    result.albums.map(o => {
+      albums.push(o.title)
+    })
+    console.log(result.nextPageToken, result.albums.length)
+
+    if (result.nextPageToken && result.albums.length > 0) {
+       await getNextAlbumPage(albums, result.nextPageToken)
+    }
+    return albums
+  }
+
+  return getNextAlbumPage([])
 }
 
-// If modifying these scopes, delete token.json.
-const SCOPES = [
-  'https://www.googleapis.com/auth/photoslibrary.readonly',
-  'https://www.googleapis.com/auth/calendar.readonly'];
+const albumList = async (accessToken, pageSize = 20) => {
+  const photos = new Photos(accessToken)
+  const albums = []
+  let hasNext = true
+  let nextPageToken = ''
 
-
-program.option('-a, --auth-code <auth>', 'Auth code from server.')
-program.parse(process.argv)
-const options = program.opts();
-
-const storage = new FileTokenStorage('token.json')
-
-authenticatedClient('credentials.json', SCOPES, storage, options.authCode).then(response => {
-  if (response.authUrl) {
-    console.log('Open URL: ', response.authUrl)
-    console.log('Run: node index.js --auth-code <code>')
-    return
-  } else {
-    listEvents(response.client)
-
-    storage.readToken()
-      .then(tokens => {
-        return listPhotos(tokens.access_token)
-      })
-      .then(response => {
-        console.log(response)
-      })
+  while (hasNext) {
+    const result = await photos.albums.list(pageSize, nextPageToken)
+    result.albums.map(o => {
+      albums.push(o)
+      console.log(o.title);
+    })
+    nextPageToken = result.nextPageToken
+    hasNext = (result.albums.length > 0) && (typeof nextPageToken != 'undefined')
+    console.log(`Got ${result.albums.length} albums. Making next request = ${hasNext}`)
   }
-})
+  console.log('end')
+
+  return albums;
+}
+
+const main = () => {
+  program.option('-a, --auth-code <auth>', 'Auth code from server.')
+  program.parse(process.argv)
+  const options = program.opts();
+  const storage = new FileTokenStorage('token.json')
+
+
+  authenticatedClient('credentials.json', SCOPES, storage, options.authCode).then(response => {
+    if (response.authUrl) {
+      console.log('Open URL: ', response.authUrl)
+      console.log('Run: node index.js --auth-code <code>')
+      return
+    } else {
+      response.client.refreshAccessToken()
+        .then(({credentials}) => {
+          storage.writeToken(credentials)
+          return albumList(credentials.access_token)
+        })
+        .then(albums => {
+          console.log(albums)
+          fs.writeFileSync('albums.json', JSON.stringify(albums))
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }
+  })
+
+}
+
+main()
+
+
