a6ada8adc20743ce4c9ef3b0abb448e5d680bb23
Alastair Tse <alastair@liquidx.net>
2021-04-03 23:25:49 +0900

    Add some basic information

---
+++ README.md
@@ -0,0 +1,17 @@
+Command line access to the [Google Photos API](https://developers.google.com/photos/library/guides/overview).
+
+```
+# Create your Google OAuth2 API Credentials
+...
+# To initialize authentication
+node main.py auth
+# To append the auth code
+node main.py auth "<authcode>"
+# To list all albums
+node main.py albums
+# To list all photos in the library.
+node main.py library
+```
+
+
+
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { authenticatedClient, FileTokenStorage } = require('liquidx-gapi-auth')
+const { authenticatedClient, FileTokenStorage } = require('liquidx-gapi-auth');
 const { program } = require('commander');
 const process = require('process');
 const Photos = require('googlephotos');
@@ -7,75 +7,125 @@ const fs = require('fs');
 const SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly'];
 
 const albumListRecursive = async (accessToken, pageSize = 50) => {
-  const photos = new Photos(accessToken)
+  const photos = new Photos(accessToken);
 
   const getNextAlbumPage = async (albums, nextPageToken) => {
-    const result = await photos.albums.list(pageSize, nextPageToken)
-    result.albums.map(o => {
-      albums.push(o.title)
-    })
-    console.log(result.nextPageToken, result.albums.length)
+    const result = await photos.albums.list(pageSize, nextPageToken);
+    result.albums.map((o) => {
+      albums.push(o.title);
+    });
+    console.log(result.nextPageToken, result.albums.length);
 
     if (result.nextPageToken && result.albums.length > 0) {
-       await getNextAlbumPage(albums, result.nextPageToken)
+      await getNextAlbumPage(albums, result.nextPageToken);
     }
-    return albums
-  }
+    return albums;
+  };
 
-  return getNextAlbumPage([])
-}
+  return getNextAlbumPage([]);
+};
 
 const albumList = async (accessToken, pageSize = 20) => {
-  const photos = new Photos(accessToken)
-  const albums = []
-  let hasNext = true
-  let nextPageToken = ''
+  const photos = new Photos(accessToken);
+  const albums = [];
+  let hasNext = true;
+  let nextPageToken = '';
 
   while (hasNext) {
-    const result = await photos.albums.list(pageSize, nextPageToken)
-    result.albums.map(o => {
-      albums.push(o)
+    const result = await photos.albums.list(pageSize, nextPageToken);
+    result.albums.map((o) => {
+      albums.push(o);
       console.log(o.title);
-    })
-    nextPageToken = result.nextPageToken
-    hasNext = (result.albums.length > 0) && (typeof nextPageToken != 'undefined')
-    console.log(`Got ${result.albums.length} albums. Making next request = ${hasNext}`)
+    });
+    nextPageToken = result.nextPageToken;
+    hasNext = result.albums.length > 0 && typeof nextPageToken != 'undefined';
+    console.log(`Got ${result.albums.length} albums. Making next request = ${hasNext}`);
   }
-  console.log('end')
+  console.log('end');
 
   return albums;
-}
+};
+
+const libraryList = async (accessToken, pageSize = 20) => {
+  const photos = new Photos(accessToken);
+  const items = [];
+  let hasNext = true;
+  let nextPageToken = '';
+
+  while (hasNext) {
+    const result = await photos.mediaItems.list(pageSize, nextPageToken);
+    result.mediaItems.map((o) => {
+      items.push(o);
+      console.log(o);
+    });
+    nextPageToken = result.nextPageToken;
+    hasNext = false // result.mediaItems.length > 0 && typeof nextPageToken != 'undefined';
+    console.log(`Got ${result.mediaItems.length} items. Making next request = ${hasNext}`);
+  }
+
+  return items;
+};
 
 const main = () => {
-  program.option('-a, --auth-code <auth>', 'Auth code from server.')
-  program.parse(process.argv)
-  const options = program.opts();
-  const storage = new FileTokenStorage('token.json')
-
-
-  authenticatedClient('credentials.json', SCOPES, storage, options.authCode).then(response => {
+  const storage = new FileTokenStorage('token.json');
+  const getClient = async (authCode) => {
+    const response = await authenticatedClient('credentials.json', SCOPES, storage, authCode);
     if (response.authUrl) {
-      console.log('Open URL: ', response.authUrl)
-      console.log('Run: node index.js --auth-code <code>')
-      return
-    } else {
-      response.client.refreshAccessToken()
-        .then(({credentials}) => {
-          storage.writeToken(credentials)
-          return albumList(credentials.access_token)
-        })
-        .then(albums => {
-          console.log(albums)
-          fs.writeFileSync('albums.json', JSON.stringify(albums))
-        })
-        .catch(err => {
-          console.error(err)
-        })
+      console.log('Open URL: ', response.authUrl);
+      console.log('Run: node main.js auth <code>');
     }
-  })
+    return response;
+  };
 
-}
+  program
+    .command('auth <authCode>')
+    .description('Apply auth code')
+    .action((authCode) => {
+      getClient(authCode).then((response) => {
+        if (!response.authUrl) {
+          console.log('Success.');
+        }
+      });
+    });
 
-main()
+  program
+    .command('albums')
+    .description('List all albums')
+    .action(() => {
+      getClient().then((response) => {
+        response.client
+          .refreshAccessToken()
+          .then(({ credentials }) => {
+            storage.writeToken(credentials);
+            return albumList(credentials.access_token);
+          })
+          .then((albums) => {
+            console.log(albums);
+            fs.writeFileSync('albums.json', JSON.stringify(albums));
+          });
+      });
+    });
 
+  program
+    .command('library')
+    .description('List all items in library')
+    .action(() => {
+      getClient().then((response) => {
+        response.client
+          .refreshAccessToken()
+          .then(({ credentials }) => {
+            storage.writeToken(credentials);
+            return libraryList(credentials.access_token);
+          })
+          .then((items) => {
+            console.log(items);
+            fs.writeFileSync('items.json', JSON.stringify(items));
+          });
+      });
+    });
 
+  program.parse(process.argv);
+
+};
+
+main();
